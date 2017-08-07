c782a702efd0db6db7135232f777dc63889a3d4a
venkateshmantha <manthavenkatesh@gmail.com>
2017-08-07 18:04:06 -0400

    enhanced gui

--- gui.py
+++ gui.py
@@ -1,98 +1,157 @@
-import os
+import os, pygame, mutagen.mp3
 from tkinter.filedialog import askdirectory
-
-import pygame
-from mutagen.id3 import ID3
 from tkinter import *
+from tkinter import messagebox
 
 root = Tk()
-root.minsize(300,300)
+root.wm_title("MusiCLI")
+index = 0
+count = 0
+pause = True
 
+topframe = Frame(root)
+topframe.pack()
+bottomframe = Frame(root)
+bottomframe.pack(side=BOTTOM)
 
-listofsongs = []
-realnames = []
+label = Label(topframe, text="Jukebox")
+label.pack()
+
+listbox = Listbox(topframe, width=80)
+listbox.pack(fill=X, side=BOTTOM)
+
+songs = []
 
 v = StringVar()
-songlabel = Label(root,textvariable=v,width=35)
+songlabel = Label(bottomframe, textvariable=v, width=80)
+songlabel.pack()
 
-index = 0
+openbutton = Button(bottomframe, text="Open")
+openbutton.pack(side=LEFT, ipadx=25)
 
-def directorychooser():
+previousbutton = Button(bottomframe, text="Prev")
+previousbutton.pack(side=LEFT, ipadx=30)
 
-    directory = askdirectory()
-    os.chdir(directory)
+playbutton = Button(bottomframe, text="Play")
+playbutton.pack(side=LEFT, ipadx=30)
 
-    for files in os.listdir(directory):
-        if files.endswith(".mp3"):
+stopbutton = Button(bottomframe, text="Stop")
+stopbutton.pack(side=LEFT, ipadx=30)
 
-            realdir = os.path.realpath(files)
-            audio = ID3(realdir)
-            realnames.append(audio['TIT2'].text[0])
+nextbutton = Button(bottomframe, text="Next")
+nextbutton.pack(side=LEFT, ipadx=30)
 
+pausebutton = Button(bottomframe, text="Pause")
+pausebutton.pack(side=LEFT, ipadx=30)
 
-            listofsongs.append(files)
-
-
-    pygame.mixer.init()
-    pygame.mixer.music.load(listofsongs[0])
-    pygame.mixer.music.play()
-
-directorychooser()
 
 def updatelabel():
     global index
-    global songname
-    v.set(realnames[index])
-    #return songname
+    v.set(songs[index])
 
+
+def pausesong(event):
+    global pause
+
+    if pause:
+        pygame.mixer.music.pause()
+        pause = False
+    else:
+        pygame.mixer.music.unpause()
+
+
+def playsong(event):
+    pygame.mixer.music.play()
 
 
 def nextsong(event):
     global index
     index += 1
-    pygame.mixer.music.load(listofsongs[index])
-    pygame.mixer.music.play()
-    updatelabel()
+    if index < count:
+        pygame.mixer.music.load(songs[index])
+        pygame.mixer.music.play()
+    else:
+        index = 0
+        pygame.mixer.music.load(songs[index])
+        pygame.mixer.music.play()
+    try:
+        updatelabel()
+    except NameError:
+        print("")
 
-def prevsong(event):
+
+def previoussong(event):
     global index
     index -= 1
-    pygame.mixer.music.load(listofsongs[index])
+    pygame.mixer.music.load(songs[index])
     pygame.mixer.music.play()
-    updatelabel()
+    try:
+        updatelabel()
+    except NameError:
+        print("")
 
 
 def stopsong(event):
     pygame.mixer.music.stop()
-    v.set("")
-
-label = Label(root,text='Music Player')
-label.pack()
-
-listbox = Listbox(root)
-listbox.pack()
-
-realnames.reverse()
-
-for items in realnames:
-    listbox.insert(0,items)
-
-realnames.reverse()
-
-nextbutton = Button(root,text = 'Next Song')
-nextbutton.pack()
-
-previousbutton = Button(root,text = 'Previous Song')
-previousbutton.pack()
-
-stopbutton = Button(root,text='Stop Music')
-stopbutton.pack()
 
 
-nextbutton.bind("<Button-1>",nextsong)
-previousbutton.bind("<Button-1>",prevsong)
-stopbutton.bind("<Button-1>",stopsong)
+def directorychooser():
+    global count
+    global index
 
-songlabel.pack()
+    directory = askdirectory()
+
+    if directory:
+        count = 0
+        index = 0
+
+        del songs[:]
+        os.chdir(directory)
+
+        for file in os.listdir(directory):
+            if file.endswith(".mp3"):
+                songs.append(file)
+
+        if len(songs) == 0:
+            retry = messagebox.askretrycancel("No songs found", "No songs found!")
+            if retry:
+                directorychooser()
+
+        else:
+            listbox.delete(0, END)
+            for song in songs:
+                listbox.insert(END, song)
+                count += 1
+
+            file = mutagen.mp3.MP3(songs[0])
+            pygame.mixer.init(frequency=file.info.sample_rate)
+            pygame.mixer.music.load(songs[0])
+            pygame.mixer.music.play()
+            try:
+                updatelabel()
+            except NameError:
+                print("")
+    else:
+        return 1
+
+
+def choosedirectory(event):
+    try:
+        directorychooser()
+    except WindowsError:
+        print("Exiting!")
+
+
+openbutton.bind("<Button-1>", choosedirectory)
+playbutton.bind("<Button-1>", playsong)
+nextbutton.bind("<Button-1>", nextsong)
+previousbutton.bind("<Button-1>", previoussong)
+stopbutton.bind("<Button-1>", stopsong)
+pausebutton.bind("<Button-1>", pausesong)
+
+try:
+    directorychooser()
+except:
+    print("Exited!")
 
 root.mainloop()
